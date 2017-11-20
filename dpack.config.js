abc2a887cca835bb59abd5e2b3b2a753f0c23def
welllee <welllee@didichuxing.com>
2017-11-20 16:41:46 +0800

    update style

--- dpack.config.js
+++ dpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
 
-module.exports = function(WebpackConfig) {
-	WebpackConfig.entry = [path.resolve(process.cwd(), 'src/bootstrap.js')]
-	WebpackConfig.resolve.alias = {
-		'api': path.resolve('./src/api'),
-		'components': path.resolve('./src/components')
-	}
-	return WebpackConfig
+module.exports = function(dpack) {
+	// dpack.set('entry', [path.resolve(process.cwd(), 'src/bootstrap.js')])
+	// dpack.set('resolve.alias', {
+	// 	'api': path.resolve('./src/api'),
+	// 	'components': path.resolve('./src/components')
+	// })
+	dpack.set({
+		'entry': [path.resolve(process.cwd(), 'src/bootstrap.js')],
+		'resolve.alias': {
+			'api': path.resolve('./src/api'),
+			'components': path.resolve('./src/components')
+		}
+	})
+	// return dpack
 }
