71f786b5538f5862be954820950c80af72e754a3
leaven <dlwelllee@gmail.com>
2018-08-10 13:49:45 +0800

    upgrade

--- dpack.config.js
+++ dpack.config.js
@@ -1,17 +1,11 @@
 const path = require('path')
 
-module.exports = function(dpack) {
-	// dpack.set('entry', [path.resolve(process.cwd(), 'src/bootstrap.js')])
-	// dpack.set('resolve.alias', {
-	// 	'api': path.resolve('./src/api'),
-	// 	'components': path.resolve('./src/components')
-	// })
-	dpack.set({
-		'entry': [path.resolve(process.cwd(), 'src/bootstrap.js')],
-		'resolve.alias': {
+module.exports = 	{
+	'entry': [path.resolve(process.cwd(), 'src/bootstrap.js')],
+	'resolve':{
+		'alias': {
 			'api': path.resolve('./src/api'),
 			'components': path.resolve('./src/components')
 		}
-	})
-	// return dpack
+	}
 }
